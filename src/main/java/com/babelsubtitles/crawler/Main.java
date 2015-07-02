ff28f7620547450d707b34ae736f9593ea907274
Francisco Javier Jimenez Saez <fjjs.01@gmail.com>
2015-07-02 19:15:47 +0200

    =Init project

---
+++ src/main/java/com/babelsubtitles/crawler/Main.java
@@ -0,0 +1,11 @@
+package com.babelsubtitles.crawler;
+
+/**
+ * Created by Javi on 02/07/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new SubtitlesCrawler().run();
+    }
+}
---
+++ src/main/java/com/babelsubtitles/crawler/SubtitlesCrawler.java
@@ -0,0 +1,10 @@
+package com.babelsubtitles.crawler;
+
+/**
+ * Created by Javi on 02/07/2015.
+ */
+public class SubtitlesCrawler {
+    public void run() {
+
+    }
+}
